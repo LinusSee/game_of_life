1e73a7231addf7e13426a9ed860a057b82eef0c1
Linus <linussee@web.de>
2019-11-23 14:44:56 +0100

    Refactored survival rule

--- python/src/Rules.py
+++ python/src/Rules.py
@@ -16,8 +16,7 @@ class SurvivalRule:
         self.alive_neighbours = alive_neighbours
 
     def applies(self):
-        if self.cell_status == True and self.alive_neighbours == 2:
-            return True
-        if self.cell_status == True and self.alive_neighbours == 3:
-            return True
+        if self.cell_status == True:
+            if self.alive_neighbours == 2 or self.alive_neighbours == 3:
+                return True
         return False
