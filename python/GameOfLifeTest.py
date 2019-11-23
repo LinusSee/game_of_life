e9a7ff2010a4f3c5f6b90a2cb61247d6b3e739cd
Linus <linussee@web.de>
2019-11-23 14:40:19 +0100

    Improved variable naming consistency
    Rules used cell_status and tests used cell_state as the name for whether
    a cell is alive or not. Changed all to cell_status

--- python/GameOfLifeTest.py
+++ python/GameOfLifeTest.py
@@ -7,26 +7,26 @@ from src.Rules import SurvivalRule
 class GameOfLifeTest(unittest.TestCase):
     def test_an_alive_cell_with_0_neighbours_dies(self):
         alive_neighbours = 0
-        cell_state = True
+        cell_status = True
 
-        actual = PopulationRule(cell_state, alive_neighbours).applies()
+        actual = PopulationRule(cell_status, alive_neighbours).applies()
 
         self.assertTrue(actual)
 
     def test_an_alive_cell_with_1_neighbour_dies(self):
         alive_neighbours = 1
-        cell_state = True
+        cell_status = True
 
-        actual = PopulationRule(cell_state, alive_neighbours).applies()
+        actual = PopulationRule(cell_status, alive_neighbours).applies()
 
         self.assertTrue(actual)
 
 
     def test_an_alive_cell_with_2_neighbours_stays_alive(self):
         alive_neighbours = 2
-        cell_state = True
+        cell_status = True
 
-        actual = SurvivalRule(cell_state, alive_neighbours).applies()
+        actual = SurvivalRule(cell_status, alive_neighbours).applies()
 
         self.assertTrue(actual)
 
