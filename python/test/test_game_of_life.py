637e1ece5ce8797db09772a5772e0d9b7848fcbe
Linus <linussee@web.de>
2019-11-24 15:40:52 +0100

    GameOfLife case1

--- python/test/test_game_of_life.py
+++ python/test/test_game_of_life.py
@@ -1,10 +1,21 @@
 import unittest
 
+from src.GameOfLife import GameBoard
 from src.GameOfLife import GameOfLife
 
 
 class GameOfLifeTest(unittest.TestCase):
-    pass
+    def test_game_returns_the_correct_board_after_creation(self):
+        board = [
+            [ True, True, False ],
+            [ False, True, True ],
+            [ False, True, False ]
+        ]
+
+        actual = GameOfLife(board).board().board
+        expected = GameBoard(board).board
+
+        self.assertEqual(actual, expected)
 
 
 if __name__ == "__main__":
