82a66b81ad5f28efbb5dbfad4d8020ae0306398a
LinusSee <linussee@web.de>
2023-11-12 18:16:05 +0100

    feat: underpopulation case 1
    Cell with 0 neighbours dies

---
+++ elm/src/Rule.elm
@@ -0,0 +1,6 @@
+module Rule exposing (cellWillBeAlive)
+
+cellWillBeAlive: Int -> Bool
+cellWillBeAlive neighbours = False
+
+
--- elm/tests/RulesTest.elm
+++ elm/tests/RulesTest.elm
@@ -1,13 +1,20 @@
 module RulesTest exposing (..)
 
 import Expect
+import Rule
 import Test exposing (..)
 
 
 rulesSuite : Test
 rulesSuite =
     describe "Testing GameOfLifeRules"
-        [ test "example" <|
-            \_ ->
-                Expect.equal "hello" "hello"
+        [ describe "underpopulation"
+            [ test "cell should die with 0 neighbours" <|
+                \_ ->
+                    let
+                        isAlive =
+                            Rule.cellWillBeAlive 0
+                    in
+                    Expect.equal False isAlive
+            ]
         ]
